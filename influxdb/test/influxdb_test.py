864b70728ce37f348633c7d9635c9e4753241e0b
Robert Hargreaves <kiyouta@gmail.com>
2025-11-25 21:52:40 +0000

    feat: test basic writing to InfluxDB 3

--- influxdb/test/influxdb_test.py
+++ influxdb/test/influxdb_test.py
@@ -1,12 +1,25 @@
-import pytest
 import os
-import influxdb_client
+from influxdb_client_3 import InfluxDBClient3, Point
 
 
-INFLUXDB_URL = os.getenv("INFLUXDB_URL")
+INFLUXDB_HOST = os.getenv("INFLUXDB_HOST")
 INFLUXDB_TOKEN = os.getenv("INFLUXDB_TOKEN")
+DB_NAME = "test-db"
 
 
-def test_influxdb_client():
-    client = influxdb_client.InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN)
-    assert client.ping()
+def test_influxdb_write():
+    client = InfluxDBClient3(
+        host=INFLUXDB_HOST,
+        token=INFLUXDB_TOKEN,
+    )
+
+    p = Point("test").tag("location", "Prague").field("value", 1)
+    client.write(database=DB_NAME, record=p.to_line_protocol())
+
+    table = client.query(
+        "SELECT * FROM test WHERE time > now() - INTERVAL '3 seconds'",
+        language="sql",
+        database=DB_NAME,
+    )
+    rows = table.to_pylist()
+    assert len(rows) == 1
